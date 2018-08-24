284180c1e8362f33fa4eb3a65a18a8a2a9158d9d
dazyang <dazyang@protonmail.com>
2018-08-24 16:40:34 +1200

    added delete route

--- routes/todos.js
+++ routes/todos.js
@@ -5,7 +5,10 @@ const router = express.Router()
 // require our model
 const db = require('../models')
 
-// when we refer to router.get /, in the main app it's actually going to be router.get/api/todos/:id (api/todos is prefixed in the main app)
+// Defining the index route
+
+// when we refer to router.get /, in the main app it's actually going to be router.get/api/todos/:id (api/todos is prefixed in the main app) 
+// GET /api/todos (List all todos)
 router.get('/', function(req, res){
   db.Todo.find() 
   //find() is a method that Mongoose is going to go connect our database to find all todos
@@ -16,7 +19,7 @@ router.get('/', function(req, res){
     res.send(err)
   })
 }) 
-
+// POST /api/todos (Create new todo
 router.post('/', function(req, res){
   //bodyparser is giving us the ability to now reference req.body
   db.Todo.create(req.body) 
@@ -28,6 +31,7 @@ router.post('/', function(req, res){
   })
 })
 
+// GET  /api/todos/:todold (Retrieve a todo)
 router.get('/:todoId', function(req, res){
   // param will be filled whatever the variables are //:define_something_as_a_path_variable
   db.Todo.findById(req.params.todoId)
@@ -39,6 +43,8 @@ router.get('/:todoId', function(req, res){
   })
 }) 
 
+
+// PUT  /api/todos/:todold (Update a todo)
 router.put('/:todoId', function(req, res){
   db.Todo.findOneAndUpdate({_id:req.params.todoId}, req.body, {new:true})
   .then(function(todo){
@@ -46,12 +52,20 @@ router.put('/:todoId', function(req, res){
   })
 })
 
+// DELETE  /api/todos/:todold (Delete a todo)
+router.delete('/:todoId', function(req, res) {
+  db.Todo.remove({_id: req.params.todoId})
+  .then(function(){
+    res.json({message: 'GONE!'})
+  })
+  .catch(function(err){
+    res.send(err)
+  })
+})
+
 
 module.exports = router
 
-// Defining the index route 
-// GET /api/todos (List all todos)
-// POST /api/todos (Create new todo)
-// GET  /api/todos/:todold (Retrieve a todo)
-// PUT  /api/todos/:todold (Update a todo)
-// DELETE  /api/todos/:todold (Delete a todo)
+
+
+
