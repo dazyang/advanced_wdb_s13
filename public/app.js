c3e6539bdf1ed2f7fa27cf6b2bed4c9d2ade32aa
dazyang <dazyang@protonmail.com>
2018-08-28 17:15:09 +1200

    included js file in html

---
+++ public/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $.getJSON("/api/todos")
+  .then(addTodos)
+})
+
+function addTodos(todos){
+  todos.forEach(function(todo){
+    const newTodo = $("<li class='task'>" + todo.name + "</li>")
+    $('.list').append(newTodo)
+  })
+}
