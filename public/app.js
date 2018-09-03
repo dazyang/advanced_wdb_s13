4cf438e3e1922bd60d4e79b39ed9aa161c1ce47f
dazyang <dazyang@protonmail.com>
2018-09-03 15:15:42 +1200

    added delete function

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+//when the page loads select the html document
 $(document).ready(function(){
   $.getJSON("/api/todos")
   .then(addTodos)
@@ -6,6 +7,10 @@ $(document).ready(function(){
     if (event.which == 13) {
       createTodo()
     }
+  })
+  //The document is listening for clicks on something that exists on the page at the beginning, within that we are looking for 'span'
+  $('.list').on('click', 'span', function(){
+     removeTodo($(this).parent())
   })
 })
 
@@ -16,7 +21,9 @@ function addTodos(todos){
 }
 
 function addTodo(todo){
-  const newTodo = $("<li class='task'>" + todo.name + "</li>")
+  const newTodo = $("<li class='task'>" + todo.name + "<span>X</span></li>")
+  // jQuery's .data method stores little pieces of data, in this case the id
+  newTodo.data('id', todo._id)
   if (todo.completed) { newTodo.addClass('done') }
   $('.list').append(newTodo)
 }
@@ -32,4 +39,19 @@ function createTodo(newTodo){
   .catch(function(err){
     console.log(err)
   })
+}
+
+function removeTodo(todo){
+  const clickedId = todo.data('id')
+  const deleteUrl = 'api/todos/' + clickedId
+  $.ajax({
+    method: 'DELETE',
+    url: deleteUrl
+  })
+    .then(function (data) {
+      todo.remove()
+    })
+    .catch(function(err){
+      console.log(err)
+    })
 }
